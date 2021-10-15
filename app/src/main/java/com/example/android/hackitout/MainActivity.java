c7f48fc3930748a6b341711de615a0cb469c6fca
saksham wane <wanesaksham@gmail.com>
2021-10-15 17:35:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/hackitout/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.android.hackitout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView javaCourse = findViewById(R.id.java_text_view);
+        javaCourse.setOnClickListener(view -> {
+            Intent javaIntent = new Intent(this, javaCourse.class);
+            startActivity(javaIntent);
+        });
+
+        TextView pythonCourse = findViewById(R.id.python_text_view);
+        pythonCourse.setOnClickListener(view -> {
+            Intent pythonIntent = new Intent(this, pythonCourse.class);
+            startActivity(pythonIntent);
+        });
+    }
+}
